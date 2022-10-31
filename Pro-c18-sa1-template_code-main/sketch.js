b9d030ed882240caa0c27ec714fa625f516c922b
RocketGuy569 <114612012+RocketGuy569@users.noreply.github.com>
2022-10-31 19:31:46 +0000

    Add files via upload

---
+++ Pro-c18-sa1-template_code-main/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(400,400);
+  s1= new Student("Vraj",843183,13,8)
+  s2= new Student("Bob",987658,11,6)
+  s3= new Student("Jeff",345678,1,12)
+  s1.display()
+  s2.display()
+  s3.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ Pro-c18-sa1-template_code-main/student.js
@@ -0,0 +1,20 @@
+
+class Student
+{
+constructor(Name,StudentNumber,Age,YearGroup) {
+   this.Name=Name
+   this.StudentNumber=StudentNumber
+   this.Age=Age
+   this.YearGroup=YearGroup
+}
+display() {
+    console.log(this.Name)
+    console.log(this.StudentNumber)
+    console.log(this.Age)
+    console.log(this.YearGroup)
+}
+    
+}
+
+
+
